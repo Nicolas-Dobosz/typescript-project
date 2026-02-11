4b855f27edecda372a35f9754c469ca9d81f8945
matthieu Laurent <ml.laurent.matthieu@gmail.com>
2026-02-11 11:16:46 +0100

    create back for the create post

--- app/components/createPost.tsx
+++ app/components/createPost.tsx
@@ -1,25 +1,43 @@
 "use client";
 import React, { useState } from "react";
+import { auth } from "../lib/auth";
 
 export default function CreatPostModal() {
-  const [content, setContent] = useState("");
-  const [url, setUrl] = useState("");
-  const [error, setError] = useState("");
+  const [content, setContent] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const user: number = auth.getUser().id;
 
   const handleClose = () => {
-    console.log("COUCOU")
-  }
+    console.log("COUCOU");
+  };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("");
 
     if (!content || !url) {
-			setError('Tous les champs sont requis');
-			return;
-		}
+      setError("Tous les champs sont requis");
+      return;
+    }
+    try {
+      const res = await fetch("/api/createPost", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user, content, url }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(data.error);
+        return;
+      }
+
+      handleClose;
+    } catch {
+      setError("Erreur serveur");
+    }
   };
-
-
 
   return (
     <div>
@@ -43,14 +61,19 @@ export default function CreatPostModal() {
             onChange={(e) => setUrl(e.target.value)}
           />
         </p>
+        {error && (
+          <div className="mb-4 p-3 bg-red-50 border border-red-200 text-red-700 rounded">
+            {error}
+          </div>
+        )}
+        <button
+          type="submit"
+          className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition"
+        >
+          Se connecter
+        </button>
+        <button onClick={handleClose}></button>
       </form>
-      <button
-        type="submit"
-        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 rounded-lg transition"
-      >
-        Se connecter
-      </button>
-      <button onClick={handleClose}></button>
     </div>
   );
 }
